a40b1547aac7198b44dc84a3921930d81be0a838
Jon Free <freejonm@gmail.com>
2021-01-15 13:43:37 -0500

    calling Open Weather API to return tomorrow's high temp in London

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-var settings = {
+var currentWeather = {
 	"async": true,
 	"crossDomain": true,
 	"url": "https://community-open-weather-map.p.rapidapi.com/weather?q=London&units=imperial",
@@ -11,8 +11,24 @@ var settings = {
 	}
 };
 
-$.ajax(settings).done(function (response) {
+var forecast = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://community-open-weather-map.p.rapidapi.com/forecast?q=london&units=imperial",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "dc28ad23b6msh135d38017d65631p1522a3jsndcf4ea383ce3",
+		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+	}
+}
+
+$.ajax(currentWeather).done(function (response) {
     console.log(response);
     $("body").append("<div>" + response.main.temp + "</div>");
+});
+
+$.ajax(forecast).done(function (response) {
+    console.log(response);
+    $("body").append("<div>" + response.list[0].main.temp_max + "</div>");
 });
 
