a5abb8f40d1ad018291a01530719acf1e653d81f
Jon Free <freejonm@gmail.com>
2021-01-15 13:31:16 -0500

    calling Open Weather API to return current temp in London, UK

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-console.log("script running");
 
-$("body").append("<div>JQUERY test</div>");
+
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://community-open-weather-map.p.rapidapi.com/weather?q=London&units=imperial",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "dc28ad23b6msh135d38017d65631p1522a3jsndcf4ea383ce3",
+		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+    console.log(response);
+    $("body").append("<div>" + response.main.temp + "</div>");
+});
+
